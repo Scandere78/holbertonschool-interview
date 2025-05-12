9e06f9efccbbefb2df0206c403b7fc63e56f615e
ScandereT78 <scanderetejpro@gmail.com>
2025-05-12 13:55:11 +0100

    fix task

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Parses stdin log lines to compute file size and HTTP status code metrics.
-"""
+"""Input stats"""
 import sys
 
 stats = {
@@ -14,11 +12,11 @@ stats = {
     '405': 0,
     '500': 0
 }
-total_size = 0
+sizes = [0]
 
 
 def print_stats():
-    print('File size: {}'.format(total_size))
+    print('File size: {}'.format(sum(sizes)))
     for s_code, count in sorted(stats.items()):
         if count:
             print('{}: {}'.format(s_code, count))
@@ -30,15 +28,14 @@ try:
         try:
             status_code = matches[-2]
             file_size = matches[-1]
-            if status_code in stats:
+            if status_code in stats.keys():
                 stats[status_code] += 1
-            total_size += int(file_size)
+            sizes.append(int(file_size))
         except Exception:
             pass
         if i % 10 == 0:
             print_stats()
     print_stats()
-
 except KeyboardInterrupt:
     print_stats()
     raise
