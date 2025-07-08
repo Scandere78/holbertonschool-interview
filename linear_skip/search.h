8bded90d53107d50e3835d16af7d7b0ad7b8ac5b
Scandere78 <scanderetejpro@gmail.com>
2025-07-08 12:21:29 +0200

    add the project linear_skip

--- linear_skip/search.h
+++ linear_skip/search.h
@@ -15,15 +15,14 @@
  * @next: Pointer to the next node
  * @express: Pointer to the next node in the express lane
  *
- * Description: singly linked list node structure with an express lane
- * for Holberton project
  */
+
 typedef struct skiplist_s
 {
-    int n;
-    size_t index;
-    struct skiplist_s *next;
-    struct skiplist_s *express;
+int n;
+size_t index;
+struct skiplist_s *next;
+struct skiplist_s *express;
 } skiplist_t;
 
 skiplist_t *create_skiplist(int *array, size_t size);
@@ -31,4 +30,4 @@ void print_skiplist(const skiplist_t *list);
 void free_skiplist(skiplist_t *list);
 skiplist_t *linear_skip(skiplist_t *head, int value);
 
-#endif /* _SEARCH_H_ */
+#endif
