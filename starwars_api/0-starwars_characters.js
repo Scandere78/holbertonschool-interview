c7ecf431659a1ffd5ba72582cae62ef7af82d0c9
Scandere78 <scanderetejpro@gmail.com>
2025-07-17 20:18:15 +0200

    add the projectlaser Starwars

---
+++ starwars_api/0-starwars_characters.js
@@ -0,0 +1,44 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+if (!movieId) {
+	console.error('Usage: ./0-starwars_characters.js <movie_id>');
+	process.exit(1);
+}
+
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const data = JSON.parse(body);
+  const characters = data.characters;
+
+  const fetchCharacter = (url) => {
+    return new Promise((resolve, reject) => {
+      request(url, (err, res, bodyChar) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(JSON.parse(bodyChar).name);
+        }
+      });
+    });
+  };
+
+  (async () => {
+    for (const url of characters) {
+      try {
+        const name = await fetchCharacter(url);
+        console.log(name);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  })();
+})
---
+++ starwars_api/README.md
@@ -0,0 +1,9 @@
+# Star Wars API - Characters
+
+This script prints all characters from a Star Wars movie using the [SWAPI](https://swapi.dev).
+
+## Usage
+
+```bash
+./0-starwars_characters.js <movie_id>
+
