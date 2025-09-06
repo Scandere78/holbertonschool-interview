b00bf3d95b5a32d9f5a8a52051f0c0161a6eb06d
Scandere78 <scanderetejpro@gmail.com>
2025-09-06 12:24:46 +0200

    add the project

--- rotate_2d_matrix/0-rotate_2d_matrix.py
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""File contening the function rotate_2d_matrix."""
+"""Given an n x n 2D matrix, rotate it 90 degrees clockwise."""
 
 
 def rotate_2d_matrix(matrix):
-    """Given an n x n 2D matrix, rotate it 90 degrees clockwise."""
-    n = len(matrix)
+    """Rotates matrix 90 degress"""
 
-    for i in range(n):
-        for j in range(i + 1, n):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    row = len(matrix)
+    column = row - 1
 
-    for row in matrix:
+    for i in range(0, int(row / 2)):
+        for j in range(i, column - i):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[column - j][i]
+            matrix[column - j][i] = matrix[column - i][column - j]
+            matrix[column - i][column - j] = matrix[j][column - i]
+            matrix[j][column - i] = temp
