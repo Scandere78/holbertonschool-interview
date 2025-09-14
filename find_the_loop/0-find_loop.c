ab7c40854a8b0a8b6c6ba659670c6f8969200448
Scandere78 <scanderetejpro@gmail.com>
2025-09-14 14:01:50 +0200

    add the project find the loop

---
+++ find_the_loop/0-find_loop.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds the loop in a linked list.
+ * @head: linked list
+ * Return: The address of the node where the loop starts,
+ * or NULL if there is no loop.
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+    listint_t *slow = head;
+	listint_t *fast = head;
+
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		{
+			slow = head;
+			while (fast != NULL)
+			{
+				if (slow == fast)
+					return (slow);
+				slow = slow->next;
+				fast = fast->next;
+			}
+		}
+	}
+	return (NULL);
+}
